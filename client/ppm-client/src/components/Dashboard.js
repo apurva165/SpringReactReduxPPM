7a73303b4d67e5e00d42ffb6a82e2ae37d275420
apurva kulkarni <apurva.kulkarni@conduent.com>
2020-04-15 18:07:48 -0500

    started frontend

---
+++ client/ppm-client/src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+import ProjectItem from './Project/ProjectItem';
+
+export default class Dashboard extends Component {
+  render() {
+    return (<div>
+      <h1>Welcome to Dashboard</h1>
+      <ProjectItem/>
+    </div>)
+  }
+}
---
+++ client/ppm-client/src/components/Layout/Header.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+//import ProjectItem from './Project/ProjectItem';
+
+export default class Header extends Component {
+  render() {
+    return (<div>
+      <h1>Header Componment</h1>
+
+    </div>)
+  }
+}
